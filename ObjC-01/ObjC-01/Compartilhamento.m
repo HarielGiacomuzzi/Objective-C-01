9f6557b5f05d0e3be887570c095f8d8155753d49
leafarmd <leafarmd@gmail.com>
2015-02-19 15:36:38 -0200

    compartilhamento criado

---
+++ ObjC-01/ObjC-01/Compartilhamento.m
@@ -0,0 +1,18 @@
+//
+//  NSObject+Compartilhamento.m
+//  ObjC-01
+//
+//  Created by Rafael on 2/19/15.
+//  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+#import "Compartilhamento.h"
+
+@implementation NSObject (Compartilhamento)
+
+-(BOOL)shareNews:(News *)NewsShared :(NSString *)socialNetworkType
+{
+    NSLog(@"%d/n%d"NewsShared.)
+}
+
+@end
